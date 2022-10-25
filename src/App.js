9d73db3e06f1dcf2d871af7c1627ed809287d9dd
David R <codingaccelerator@gmail.com>
2022-10-25 20:58:47 +0200

    ✨ F01: MOVIE > View data and manage routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/home/Home';
+import MovieCard from './components/pages/movie/MovieCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<MovieCard />} />
+      </Routes>
     </div>
   );
 }
